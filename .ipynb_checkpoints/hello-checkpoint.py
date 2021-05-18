ed0a44155d3d2badfc9d3e59d951e359502fa84d
skylerhar1 <hars@oregonstate.edu>
2021-05-18 21:38:53 +0000

    added a comment-skyler

--- .ipynb_checkpoints/hello-checkpoint.py
+++ .ipynb_checkpoints/hello-checkpoint.py
@@ -2,4 +2,3 @@ def saying_hello():
     print('Just wanted to say hello to my collaborator')
     for i in range(20):
         print("hello")
-#this is a comment on how git resolved conficts-skyler:
